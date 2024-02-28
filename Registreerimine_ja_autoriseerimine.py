803e91aba2e04887b029dd7d0923662db6743a21
opilane <opilane@HP-CZC2349HT9>
2024-02-28 16:00:01 +0200

    Add project files.

---
+++ Registreerimine_ja_autoriseerimine.py
@@ -0,0 +1,14 @@
+﻿from module1 import*
+login=[]
+parool=[]
+while True:
+    print("0-Registreerimine\n1-Autoriseerimine\n2-nime või parooli muutmine\n3-unustanud parooli taastamine\n4-lõpetamine\n")
+    vastus=int(input())
+    if vastus==0:
+        new_login_parool=create_login_parool(login, parool)
+    #elif vastus==1:
+    #elif vastus==2:
+    #elif vastus==3:
+    elif vastus==4:
+        print("lõpetamine...")
+        lopp()
---
+++ module1.py
@@ -0,0 +1,30 @@
+﻿import sys
+import random
+def lopp():
+    """Заканчиваем работу приложения
+    """
+    sys.exit()
+    return
+def create_login_parool(login, parool):
+        a=input("Введите логин: ")
+        if a in login:
+            print("Такой логин уже существует. Заново.")
+        else:
+            login.append(a)
+            print("Логин создан.", a)           
+            str0=".,:;!_*-+()/#¤%&"
+            str1 = '0123456789'
+            str2 = 'qwertyuiopasdfghjklzxcvbnm'
+            str3 = str2.upper()
+            #print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+            str4 = str0+str1+str2+str3
+            #print(str4)
+            ls = list(str4)
+            #print(ls)
+            random.shuffle(ls)
+            #print(ls)
+            # Извлекаем из списка 12 произвольных значений
+            psword = ''.join([random.choice(ls) for x in range(12)])
+            # Пароль готов
+            print("Ваш пароль. ", psword)
+            parool.append(psword)
